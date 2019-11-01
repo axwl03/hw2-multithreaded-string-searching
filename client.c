29f526041276bcd78f1e600f40e424a4df2fe0be
axwl03 <arcticwarfare2711@gmail.com>
2019-11-01 17:21:53 +0800

    finish client side interaction and query message parsing

--- client.c
+++ client.c
@@ -2,5 +2,67 @@
 
 int main(int argc, char *argv[])
 {
-
+    if(argc != 5)
+    {
+        printf("command format should be: ./client -h [IP] -p [port]\n");
+        exit(1);
+    }
+    char buffer[129], ch;
+    while(scanf(" %128[^ \n]", buffer))
+    {
+        if(strcmp(buffer, "Query") == 0)
+        {
+            while(1)
+            {
+                ch = getchar();
+                if(ch == '"')
+                {
+                    memset(buffer, 0, sizeof(buffer));
+                    if(scanf("%128[^\"\n]", buffer))
+                    {
+                        ch = getchar();
+                        if(ch == '"')
+                        {
+                            //send_request(buffer);
+                            printf("%s\n", buffer);
+                        }
+                        else if(ch == '\n')
+                        {
+                            printf("QUERY_STRING should be enclose by \"\"\n");
+                            break;
+                        }
+                        else
+                        {
+                            printf("QUERY_STRING size > 128\n");
+                            while((ch = getchar()) != '\n');
+                            break;
+                        }
+                    }
+                }
+                else if(ch == ' ')
+                    continue;
+                else if(ch == '\n')
+                    break;
+                else
+                {
+                    printf("QUERY_STRING should be enclose by \"\"\n");
+                    while((ch = getchar()) != '\n');
+                    break;
+                }
+            }
+        }
+        else if(strcmp(buffer, "exit") == 0)
+            break;
+        else
+        {
+            printf("command not recognized\n");
+            while((ch = getchar()) != '\n');
+        }
+        memset(buffer, 0, sizeof(buffer));
+    }
+    return 0;
+}
+void send_request(char str)
+{
+    //int sockfd, portno;
 }
--- client.h
+++ client.h
@@ -1,4 +1,13 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+void send_request(char str);
 
 #endif
